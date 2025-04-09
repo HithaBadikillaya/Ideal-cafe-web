63e193cb1a1868164029af06cac1c9415bd44cf9
Hitha <hbadikillaya@gmail.com>
2025-04-09 10:00:57 +0530

    react template

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Carousel from "./pages/Carousel";
 
 function App() {
-
   return (
-    <>
-      
+    <Router>
+      <Routes>
+        <Route path="/" element={<Carousel />} />
         
-    </>
-  )
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
